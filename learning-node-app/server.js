6f54546ab78d36832aacb09de528e5fd05725faf
Glenn <waltandmartha@gmail.com>
2018-06-04 15:40:12 +1000

    messages working

--- learning-node-app/server.js
+++ learning-node-app/server.js
@@ -1,7 +1,26 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 
-app.use(express.static(__dirname)) 
+
+app.use(express.static(__dirname))
+app.use(bodyParser.json()) 
+app.use(bodyParser.urlencoded({extended: false}))
+
+const messages = [
+    {name: 'Glenn', message: "Hi everyone!!"},
+    {name: 'Anna', message: "What's up?!?"}
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body)
+    res.sendStatus(200)
+})
+
 const server = app.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 }) 
