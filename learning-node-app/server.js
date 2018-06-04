ef66aab6b9891a07c578bd1c57a9e7968c47c741
Glenn <waltandmartha@gmail.com>
2018-06-04 16:18:07 +1000

    add mongoose

--- learning-node-app/server.js
+++ learning-node-app/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 var bodyParser = require('body-parser')
 const app = express()
-
+const http = require('http').Server(app) //for socket.io
+const io = require('socket.io')(http)
+const mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json()) 
@@ -18,9 +20,15 @@ app.get('/messages', (req, res) => {
 
 app.post('/messages', (req, res) => {
     messages.push(req.body)
+    io.emit('message', req.body)
     res.sendStatus(200)
+
 })
 
-const server = app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
+
+const server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 }) 
